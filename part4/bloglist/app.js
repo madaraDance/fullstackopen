f8a864bf8ad399d9c05c13a3889324fa067b859d
madaraDance <maksim.minenko97@gmail.com>
2024-08-05 15:36:46 +0300

    updates for end to end testing

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const config = require('./utils/config')
 const blogPostsRouter = require('./controllers/blogposts')
 const userRouter = require('./controllers/users')
+const testingRouter = require('./controllers/testing')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const loginRouter = require('./controllers/login')
@@ -34,6 +35,11 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogPostsRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 
 app.use(middleware.unknownEndpoint)
